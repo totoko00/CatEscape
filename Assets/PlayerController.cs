3e72d0e3b3f87f833778b0c7e38d1543a3b3efe3
totoko <totoko.staphi@gmail.com>
2020-04-07 17:04:47 +0900

    完成

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -13,6 +13,26 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //左矢印押下
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.Translate(-3, 0, 0); //左に３動かす
+        }
+
+        //右矢印押下
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.Translate(3, 0, 0); //右に３動かす
+        }
+    }
+
+    public void LButtonDown()
+    {
+        transform.Translate(-3, 0, 0);
+    }
+
+    public void RButtonDown()
+    {
+        transform.Translate(3, 0, 0);
     }
 }
